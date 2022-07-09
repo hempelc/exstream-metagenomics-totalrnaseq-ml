b15a1578204a6aff41d1d87721a693278492a84b
hempelc <hempelc@uoguelph.ca>
2022-07-09 13:48:36 -0400

    polished

--- chorddiagram.R
+++ chorddiagram.R
@@ -1,20 +1,20 @@
 library('circlize')
 
-dffile = "filepath"
-outfile = "outpath"
+dffile = "/Users/christopherhempel/Desktop/overlap_df.csv"
+outfile = "/Users/christopherhempel/Desktop/chorddiagram.svg"
 
-df = data.frame(from = c("its-esv", "its-otu", "16s-esv", "16s-otu", "metagenomics", "totalrnaseq", "its-otu", "16s-esv", "16s-otu", "metagenomics", "totalrnaseq", "16s-esv", "16s-otu", "metagenomics", "totalrnaseq", "16s-otu", "metagenomics", "totalrnaseq", "metagenomics", "totalrnaseq", "totalrnaseq"),
-                to = c(rep("its-esv", 6), rep("its-otu", 5), rep("16s-esv", 4), rep("16s-otu", 3), rep("metagenomics", 2), rep("totalrnaseq", 1)),
-                value = c(20, 2, 3, 4, 5, 6, 20, 2, 3, 4,5,20,2,3,4,20,2,3,20,2,20))
+df = read.csv(dffile)
 
 grid.col <- c("its-esv" = "#aaa1ff", "its-otu" = "#2e7500", "16s-esv" = "#002a90",
               "16s-otu" = "#88e8a6", "metagenomics" = "#a9006b", "totalrnaseq" = "#d4631c")
 
 circos.par(gap.after = c(rep(10, 6)))
 
-svg(outfile)
+svg(outfile, width=3, height=3)
 
-chordDiagram(df, grid.col = grid.col, annotationTrack = c("name","grid"))
+chordDiagram(df, grid.col = grid.col, annotationTrack = c("name","grid"), 
+             order = c("metagenomics", "totalrnaseq", "its-otu", "its-esv", "16s-esv", "16s-otu"))
 
 # Close the graphics device
 dev.off()
+
