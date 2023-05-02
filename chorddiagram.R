d08451c595d4b8695bbe5b719d7977d29cfdc59d
hempelc <hempelc@uoguelph.ca>
2023-05-02 09:56:22 -0400

    Cleanup

--- chorddiagram.R
+++ chorddiagram.R
@@ -1,19 +1,20 @@
 library('circlize')
 library('glue')
 
-# Phylum or species
-rank="species"
+# Phylum or species/genus
+rank="phylum"
 
 # Set in and out file names
 dffile = glue("/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/data_files_for_ml/overlap_df_{rank}.csv")
 outfile = glue("/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/exstream_data_visualization_outdir/chorddiagram_{rank}.svg")
 
 # Read in file
+## Note: sort df manually before loading it in so that self-connections always come first
 df = read.csv(dffile)
 
 # Set colors
-grid.col <- c("its-esv" = "#aaa1ff", "its-otu" = "#2e7500", "16s-esv" = "#002a90",
-              "16s-otu" = "#88e8a6", "metagenomics" = "#a9006b", "totalrnaseq" = "#d4631c")
+grid.col <- c("ITS-2 ESV" = "#aaa1ff", "ITS-2 OTU" = "#2e7500", "16S ESV" = "#002a90",
+              "16S OTU" = "#88e8a6", "Metagenomics" = "#a9006b", "Total RNA-Seq" = "#d4631c")
 
 # Set Chord diagram gap options
 circos.par(gap.after = c(rep(10, 6)))
@@ -22,8 +23,15 @@ circos.par(gap.after = c(rep(10, 6)))
 svg(outfile, width=3, height=3)
 
 # Make Chord diagram
-chordDiagram(df, grid.col = grid.col, annotationTrack = c("name","grid"), 
-             order = c("metagenomics", "totalrnaseq", "its-otu", "its-esv", "16s-esv", "16s-otu"))
+chordDiagram(df, grid.col = grid.col, self.link = 1, link.sort = "asis",   
+             order = c("Metagenomics", "Total RNA-Seq", "ITS-2 OTU", "ITS-2 ESV", "16S ESV", "16S OTU"), annotationTrack = c("grid"), annotationTrackHeight = mm_h(5))
+
+for(si in get.all.sector.index()) {
+  xlim = get.cell.meta.data("xlim", sector.index = si, track.index = 1)
+  ylim = get.cell.meta.data("ylim", sector.index = si, track.index = 1)
+  circos.text(mean(xlim), mean(ylim), si, sector.index = si, track.index = 1, 
+              facing = "bending.inside", niceFacing = TRUE, col = "white")
+}
 
 # Close the graphics device
 dev.off()
--- data_processing_chorddiagram.py
+++ data_processing_chorddiagram.py
@@ -10,9 +10,12 @@ import plotly.express as px
 
 # Set variables
 figure_outdir = "/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/exstream_data_visualization_outdir"
-taxa_list_dic_file = "/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/data_files_for_ml/taxa_lists.pickle"
-NCBI_scientific_file="/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/data_files_for_ml/NCBI_staxids_scientific.txt"
-NCBI_non_scientific_file="/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/data_files_for_ml/NCBI_staxids_non_scientific.txt"
+indir = "/Users/christopherhempel/Google Drive/PhD UoG/ExStream project/data_files_for_ml"
+taxa_list_dict_file = "taxa_lists.pickle"
+NCBI_scientific_file="NCBI_staxids_scientific.txt"
+NCBI_non_scientific_file="NCBI_staxids_non_scientific.txt"
+nodes_dmp_file = "nodes.dmp"
+division_dmp_file = "division.dmp"
 
 # Define function to get dictinory keys from values
 def key_from_value(dict, value):
@@ -24,75 +27,138 @@ if not os.path.exists(figure_outdir):
 
 # Read in files
 ## Taxon lists
-with open(taxa_list_dic_file, 'rb') as handle:
-    taxa_list_dic = pickle.load(handle)
-# NCBI scientific names file as dictionary
-NCBI_scientific = open(NCBI_scientific_file,'r')
-## Set all file content to lowercase, so that matching of the files later is not
-## depending on upper- or lowercase:
+with open(os.path.join(indir, taxa_list_dict_file), 'rb') as handle:
+    taxa_list_dict = pickle.load(handle)
+### NCBI scientific names file as dictionary
+NCBI_scientific = open(os.path.join(indir, NCBI_scientific_file),'r')
+#### Set all file content to lowercase, so that matching of the files later is not
+#### depending on upper- or lowercase:
 NCBI_scientific_lower = (line.lower() for line in NCBI_scientific)
 reader1=csv.reader(NCBI_scientific_lower, delimiter='\t')
 NCBI_scientific_dict={}
 for row in reader1:
 	NCBI_scientific_dict[row[0]]=row[1]
-# NCBI non-scientific names file as dictionary
-NCBI_non_scientific = open(NCBI_non_scientific_file,'r')
-## Set all file content to lowercase, so that matching of the files later is not
-## depending on upper- or lowercase:
+### NCBI non-scientific names file as dictionary
+NCBI_non_scientific = open(os.path.join(indir, NCBI_non_scientific_file),'r')
+#### Set all file content to lowercase, so that matching of the files later is not
+#### depending on upper- or lowercase:
 NCBI_non_scientific_lower = (line.lower() for line in NCBI_non_scientific)
 reader2=csv.reader(NCBI_non_scientific_lower, delimiter='\t')
 NCBI_non_scientific_dict={}
 for row in reader2:
 	NCBI_non_scientific_dict[row[0]]=row[1]
 
+## Files for counting
+nodes_df = pd.read_csv(os.path.join(indir, nodes_dmp_file), sep="\t", usecols=[0,8], names=["tax_id", "division_id"], header=None, dtype=str)
+division_df = pd.read_csv(os.path.join(indir, division_dmp_file), sep="\t", header=None, usecols=[0,4], names=["division_id", "division"], dtype=str)
+tax_id_division_df = pd.merge(left=nodes_df, right=division_df, on="division_id").drop("division_id", axis=1)
+
 
 # Translate SILVA (16S) and UNITE (ITS) taxonomy to NCBI taxonomy
-## Do for lowest and highest rank (phylum and species)
-for rank in ["phylum", "species"]:
+for rank in ["phylum", "genus", "species"]:
     ### Get name of all seqtypes in taxa list
-    seqtypes_chord = taxa_list_dic[rank].keys()
-    ### Check taxa names against NCBI and only keep if match found, replace original taxa by checked taxa
+    seqtypes_chord = taxa_list_dict[rank].keys()
+    ### Check taxa names against NCBI and only keep if match found, replace original taxa by checked taxa.
+    ### Also keep taxids so that domains can be summarized later
     for seqtype_chord in seqtypes_chord:
-        taxon_lst = []
-        for taxon in taxa_list_dic[rank][seqtype_chord]:
+        taxon_dic = {}
+        for taxon in taxa_list_dict[rank][seqtype_chord]:
             if taxon.lower() in NCBI_scientific_dict:
-                taxon_lst.append(taxon.lower())
+                taxon_dic[taxon.lower()] = NCBI_scientific_dict[taxon.lower()]
             elif taxon.lower() in NCBI_non_scientific_dict:
+                #### There are two phyla that have non-scientific names without
+                #### a taxid in the scientific file, so we are replacing the two
+                #### phyla names manually with the scientific ones
                 try:
-                    translated_taxon = key_from_value(NCBI_scientific_dict, NCBI_non_scientific_dict[taxon.lower()])
-                    taxon_lst.append(translated_taxon.lower())
+                    translated_taxon = key_from_value(NCBI_scientific_dict, NCBI_non_scientific_dict[taxon.lower()]).lower()
                 except:
-                    taxon_lst.append(taxon.lower())
-        taxa_list_dic[rank][seqtype_chord] = taxon_lst
-
-## Manual addition - taxa that are spelled differently or not present on NCBI
-## were dropped from 16S and ITS, so we add them manually
-taxa_list_dic["phylum"]["otu_its_taxa"].extend(["monoblepharidomycetes", "basidiobolomycetes"])
-taxa_list_dic["phylum"]["esv_its_taxa"].extend(["monoblepharidomycetes"])
-taxa_list_dic["phylum"]["otu_16s_taxa"].extend(["candidatus cloacimonetes",
-    "candidate division fcpu426", "candidate division lcp-89", "candidatus latescibacteria",
-    "deltaproteobacteria", "candidatus eremiobacteraeota"])
-taxa_list_dic["phylum"]["esv_16s_taxa"].extend(["candidate division fcpu426",
-    "candidate division lcp-89", "candidatus latescibacteria", "deltaproteobacteria",
-    "candidatus eremiobacteraeota"])
-taxa_list_dic["species"]["otu_16s_taxa"].extend(['arcobacter suis'])
-taxa_list_dic["species"]["esv_16s_taxa"].extend(['arcobacter suis'])
-taxa_list_dic["species"]["esv_its_taxa"].extend(["orbilia oligospora",
-    "chordomyces antarcticum", "teunia cuniculi", "helicodendron westerdijkae",
-    "hongkongmyces thailandica", "articulospora tetracladia", 'monocillium sp. \'griseo-ochraceum\'',
-    "neosetophoma samarorum", 'papiliotrema sp. \'fusca\'', "aspergillus penicillioides",
-    "phomatospora dinemasporium", "pseudohalonectria adversaria", "thelephora alnii"])
-taxa_list_dic["species"]["otu_its_taxa"].extend(["orbilia oligospora", 'blastobotrys niveus',
-    '[candida] nanaspora', 'chalara heteroderae', 'chordomyces antarcticum', 'teunia cuniculi',
-    'dactylaria mitrata', 'gyrophanopsis polonensis', 'helicodendron westerdijkae',
-    'hongkongmyces thailandica', 'monocillium sp. \'griseo-ochraceum\''
-    'alnicola alnetorum', 'neoconiothyrium hakeae', "neosetophoma samarorum",
-    "aspergillus penicillioides", "phomatospora dinemasporium", 'remleria tetraspora',
-    "thelephora alnii"])
+                    if taxon.lower() == "gemmatimonadota":
+                        translated_taxon = "gemmatimonadetes"
+                    elif taxon.lower() == "elusimicrobiota":
+                        translated_taxon = "elusimicrobia"
+                taxon_dic[translated_taxon] = NCBI_non_scientific_dict[taxon.lower()]
+        #### Remove all candidate taxa for uniformity
+        taxon_dict = {k:v for k,v in taxon_dic.items() if 'candidatus' not in k if 'candidate' not in k if '[candida]' not in k}
+        taxa_list_dict[rank][seqtype_chord] = taxon_dict
 
 
-# Do for lowest and highest rank (phylum and species)
-for rank in ["phylum", "species"]:
+# Identify taxa ids with no division
+no_ids_df = pd.DataFrame(columns=["rank", "dataset", "ids"])
+for rank in ["phylum", "genus", "species"]:
+    for seqtype_chord in taxa_list_dict[rank].keys():
+        ## Assign divisions to all taxa
+        taxid_df_tmp = pd.DataFrame(taxa_list_dict[rank][seqtype_chord], index=["tax_id"]).transpose().reset_index().drop("index", axis=1)
+        tax_id_division_df_tmp = pd.merge(left=taxid_df_tmp, right=tax_id_division_df, on="tax_id", how="left")
+        ## Save rows with NA
+        no_ids_df_tmp = tax_id_division_df_tmp[tax_id_division_df_tmp["division"].isna()]
+        no_ids_df.loc["{}_{}".format(rank, seqtype_chord)] = [rank, seqtype_chord, list(no_ids_df_tmp["tax_id"])]
+
+
+# Manual addition - taxa that are spelled differently or not present on NCBI
+# were dropped from 16S and ITS, so we add them manually.
+# We also manually add taxa that have no division
+## Phylum
+taxa_list_dict["phylum"]["otu_its_taxa"] = {**taxa_list_dict["phylum"]["otu_its_taxa"], "monoblepharidomycetes": "451454", "basidiobolomycetes": "1399768"}
+taxa_list_dict["phylum"]["esv_its_taxa"]["monoblepharidomycetes"] = "451454"
+taxa_list_dict["phylum"]["esv_16s_taxa"] = {**taxa_list_dict["phylum"]["esv_16s_taxa"], "deltaproteobacteria": "28221", 'thermodesulfobacteria': "200940"}
+taxa_list_dict["phylum"]["otu_16s_taxa"]["deltaproteobacteria"] = "28221"
+
+## Genus
+taxa_list_dict["genus"]["otu_16s_taxa"]['arcobacter'] = "28196"
+taxa_list_dict["genus"]["esv_16s_taxa"]['arcobacter'] = "28196"
+taxa_list_dict["genus"]["esv_its_taxa"] = {**taxa_list_dict["genus"]["esv_its_taxa"], "orbilia": "47022",
+    "chordomyces": "1654916", "teunia": "2724800", "helicodendron": "319381",
+    "hongkongmyces": "1545378", "articulospora": "253308", 'monocillium': "581188",
+    "neosetophoma": "798068", 'papiliotrema': "189450", "aspergillus": "5052",
+    "phomatospora": "189357", "pseudohalonectria": "40655", "thelephora": "56489"}
+taxa_list_dict["genus"]["otu_its_taxa"] = {**taxa_list_dict["genus"]["otu_its_taxa"], "orbilia": "47022", 'blastobotrys': "43971",
+    'chalara': "13000", "chordomyces": "1654916", "teunia": "2724800",
+    'dactylaria': "47274", 'gyrophanopsis': "2789184", "helicodendron": "319381",
+    "hongkongmyces": "1545378", 'monocillium': "581188",
+    'alnicola': "91662", 'neoconiothyrium': "2093505", "neosetophoma": "798068",
+    "aspergillus": "5052", "phomatospora": "189357", 'remleria': "1776023",
+    "thelephora": "56489", "graphium": "1503932", "ellisembia": "375389"}
+taxa_list_dict["genus"]["dna_taxa"]["anceyoconcha"] = "2876047"
+taxa_list_dict["genus"]["rna_taxa"]["anceyoconcha"] = "2876047"
+
+## Species
+taxa_list_dict["species"]["otu_16s_taxa"]['arcobacter suis'] = "1278212"
+taxa_list_dict["species"]["esv_16s_taxa"]['arcobacter suis'] = "1278212"
+taxa_list_dict["species"]["esv_its_taxa"] = {**taxa_list_dict["species"]["esv_its_taxa"], "orbilia oligospora": "2813651",
+    "chordomyces antarcticum": "1525356", "teunia cuniculi": "365330", "helicodendron westerdijkae": "409251",
+    "hongkongmyces thailandica": "2059664", "articulospora tetracladia": "253309", 'monocillium sp. \'griseo-ochraceum\'': "2075169",
+    "neosetophoma samarorum": "798069", 'papiliotrema sp. \'fusca\'': "192649", "aspergillus penicillioides": "41959",
+    "phomatospora dinemasporium": "2718578", "pseudohalonectria adversaria": "1953033", "thelephora alnii": "56491", "niesslia mucida": "3029106"}
+taxa_list_dict["species"]["otu_its_taxa"] = {**taxa_list_dict["species"]["otu_its_taxa"], "orbilia oligospora": "2813651", 'blastobotrys niveus': "44073",
+    'chalara heteroderae': "2981829", 'chordomyces antarcticum': "1525356", 'teunia cuniculi': "365330",
+    'dactylaria mitrata': "228885", 'gyrophanopsis polonensis': "2789189", 'helicodendron westerdijkae': "409251",
+    'hongkongmyces thailandica': "2059664", 'monocillium sp. \'griseo-ochraceum\'': "2075169",
+    'alnicola alnetorum': "242214", 'neoconiothyrium hakeae': "2093506", "neosetophoma samarorum": "798069",
+    "aspergillus penicillioides": "41959", "phomatospora dinemasporium": "2718578", 'remleria tetraspora': "1776023",
+    "thelephora alnii": "56491", "niesslia mucida": "3029106"}
+taxa_list_dict["species"]["dna_taxa"]['streptomyces noursei'] = "1971"
+taxa_list_dict["species"]["rna_taxa"]['streptomyces noursei'] = "1971"
+
+
+# Count how many bacteria, archaea, and eukaryotes are in each dataset
+# Therefore, use the nodes.dmp and division.dmp from NCBI taxdump
+division_count_df = pd.DataFrame(columns=["rank", "dataset", "division", "count"])
+for rank in ["phylum", "genus", "species"]:
+    for seqtype_chord in taxa_list_dict[rank].keys():
+        ## Assign divisions to all taxa
+        taxid_df_tmp = pd.DataFrame(taxa_list_dict[rank][seqtype_chord], index=["tax_id"]).transpose().reset_index().drop("index", axis=1)
+        tax_id_division_df_tmp = pd.merge(left=taxid_df_tmp, right=tax_id_division_df, on="tax_id", how="left")
+        ## Exclude rows with NA
+        tax_id_division_df_tmp = tax_id_division_df_tmp[~tax_id_division_df_tmp["division"].isna()]
+        ## Count divisions in each dataset and add info to df
+        for div in division_df["division"]:
+            counter = tax_id_division_df_tmp["division"].str.count(div).sum()
+            division_count_df.loc["{}_{}_{}".format(rank, seqtype_chord, div)] = [rank, seqtype_chord, div, counter]
+division_count_df.to_csv(os.path.join(indir, "division_counts.csv".format(rank)), index=False)
+
+
+# Do the following for lowest and highest rank (phylum and species/genus)
+for rank in ["phylum", "genus", "species"]:
     ## First, get number of overlapping taxa between seqtypes
     ### Get all possible combinations of seqtypes
     combos = list(itertools.combinations(seqtypes_chord, 2))
@@ -102,8 +168,8 @@ for rank in ["phylum", "species"]:
     to_lst = []
     overlap_lst = []
     for i in range(len(combos)):
-        set1 = set(taxa_list_dic[rank][combos[i][0]])
-        set2 = set(taxa_list_dic[rank][combos[i][1]])
+        set1 = set(taxa_list_dict[rank][combos[i][0]])
+        set2 = set(taxa_list_dict[rank][combos[i][1]])
         overlap = len(set1 & set2)
         from_lst.append(combos[i][0])
         to_lst.append(combos[i][1])
@@ -111,41 +177,54 @@ for rank in ["phylum", "species"]:
 
     ### Turn into df and change seqtype names for consistency
     overlap_df = pd.DataFrame({"from": from_lst, "to": to_lst, "overlap": overlap_lst})
-    overlap_df["from"] = overlap_df["from"].str.replace("dna_taxa", "metagenomics").str.replace("rna_taxa", "totalrnaseq")\
-        .str.replace("esv_16s_taxa", "16s-esv").str.replace("esv_its_taxa", "its-esv").str.replace("otu_16s_taxa", "16s-otu").str.replace("otu_its_taxa", "its-otu")
-    overlap_df["to"] = overlap_df["to"].str.replace("dna_taxa", "metagenomics").str.replace("rna_taxa", "totalrnaseq")\
-        .str.replace("esv_16s_taxa", "16s-esv").str.replace("esv_its_taxa", "its-esv").str.replace("otu_16s_taxa", "16s-otu").str.replace("otu_its_taxa", "its-otu")
+    overlap_df = overlap_df.sort_values(["from", "overlap"], ascending=False)
 
-    ### Save for manual processing in R
-    overlap_df.to_csv(os.path.join(figure_outdir, "overlap_df_{0}.csv".format(rank)), index=False)
-
-
-    ### Second, get unique number of taxa for each seqtype as bubble plot
-    seqtype_chord_lst = []
+    ### Second, get total and unique number of taxa for each seqtype
+    overlap_df_unique = pd.DataFrame(columns = ["from", "to", "overlap"])
     unique_lst = []
     total_lst = []
+    seqtype_chord_lst = []
     for seqtype_chord in seqtypes_chord:
         seqtype_chord_lst.append(seqtype_chord)
-        total_lst.append(len(taxa_list_dic[rank][seqtype_chord]))
+        total_taxa = len(taxa_list_dict[rank][seqtype_chord])
+        total_lst.append(total_taxa)
         other_seqtypes = [x for x in seqtypes_chord if x != seqtype_chord]
         other_taxa = []
         for other_seqtype in other_seqtypes:
-            other_taxa.extend(taxa_list_dic[rank][other_seqtype])
+            other_taxa.extend(taxa_list_dict[rank][other_seqtype])
         other_taxa = set(other_taxa)
-        unique_taxa = len([x for x in taxa_list_dic[rank][seqtype_chord] if x not in other_taxa])
+        unique_taxa = len([x for x in taxa_list_dict[rank][seqtype_chord] if x not in other_taxa])
         unique_lst.append(unique_taxa)
+        nonunique_taxa = total_taxa - unique_taxa
+        # Determine factor by which unique taxa have to be multiplied for size ratio
+        overlap_sum = overlap_df[overlap_df.apply(lambda r: r.str.contains(seqtype_chord, case=False).any(), axis=1)]["overlap"].sum()
+        size_factor = overlap_sum/nonunique_taxa
+        unique_taxa = unique_taxa * size_factor
+        # Add to overlap df unique
+        overlap_df_unique.loc[len(overlap_df_unique)] = [seqtype_chord, seqtype_chord, unique_taxa]
 
-    ### Turn into df and change setype names for consistency
+    ## Organize df
+    overlap_df = pd.concat([overlap_df_unique, overlap_df])
+    overlap_df["from"] = overlap_df["from"].str.replace("dna_taxa", "Metagenomics").str.replace("rna_taxa", "Total RNA-Seq")\
+        .str.replace("esv_16s_taxa", "16S ESV").str.replace("esv_its_taxa", "ITS-2 ESV").str.replace("otu_16s_taxa", "16S OTU").str.replace("otu_its_taxa", "ITS-2 OTU")
+    overlap_df["to"] = overlap_df["to"].str.replace("dna_taxa", "Metagenomics").str.replace("rna_taxa", "Total RNA-Seq")\
+        .str.replace("esv_16s_taxa", "16S ESV").str.replace("esv_its_taxa", "ITS-2 ESV").str.replace("otu_16s_taxa", "16S OTU").str.replace("otu_its_taxa", "ITS-2 OTU")
+
+    ### Save for manual processing in R
+    overlap_df.to_csv(os.path.join(indir, "overlap_df_{0}.csv".format(rank)), index=False)
+
+    ### Finally, make a bubble plot for number of taxa
+    #### Turn count lists df and change setype names for consistency
     count_df = pd.DataFrame({"seqtype": seqtype_chord_lst, "unique_taxa": unique_lst, "total_taxa": total_lst})
     count_df["seqtype"] = count_df["seqtype"].str.replace("dna_taxa", "metagenomics").str.replace("rna_taxa", "totalrnaseq")\
         .str.replace("esv_16s_taxa", "16s-esv").str.replace("esv_its_taxa", "its-esv").str.replace("otu_16s_taxa", "16s-otu").str.replace("otu_its_taxa", "its-otu")
 
-    ### Change df format from wide to narrow
+    #### Change df format from wide to narrow
     count_df_narrow = pd.DataFrame({"seqtype": list(count_df["seqtype"])+list(count_df["seqtype"]),
         "taxa_cat": ["unique_taxa"]*len(count_df)+["total_taxa"]*len(count_df),
         "count": list(count_df["unique_taxa"])+list(count_df["total_taxa"])})
 
-    ### Manually make reference bubbles for phylum and species:
+    #### Manually make reference bubbles for phylum and species:
     if rank=="phylum":
         count_df_narrow = count_df_narrow.append({'seqtype': 'ref1', 'taxa_cat': 'unique_taxa', 'count': 10}, ignore_index = True)
         count_df_narrow = count_df_narrow.append({'seqtype': 'ref2', 'taxa_cat': 'unique_taxa', 'count': 50}, ignore_index = True)
@@ -158,7 +237,14 @@ for rank in ["phylum", "species"]:
         count_df_narrow = count_df_narrow.append({'seqtype': 'ref1', 'taxa_cat': 'total_taxa', 'count': 2500}, ignore_index = True)
         count_df_narrow = count_df_narrow.append({'seqtype': 'ref2', 'taxa_cat': 'total_taxa', 'count': 4000}, ignore_index = True)
 
-    ### Make figure --> add bubbles to chorddiagram manually generated in R
+    elif rank=="genus":
+        count_df_narrow = count_df_narrow.append({'seqtype': 'ref1', 'taxa_cat': 'unique_taxa', 'count': 100}, ignore_index = True)
+        count_df_narrow = count_df_narrow.append({'seqtype': 'ref2', 'taxa_cat': 'unique_taxa', 'count': 1000}, ignore_index = True)
+        count_df_narrow = count_df_narrow.append({'seqtype': 'ref1', 'taxa_cat': 'total_taxa', 'count': 2000}, ignore_index = True)
+        count_df_narrow = count_df_narrow.append({'seqtype': 'ref2', 'taxa_cat': 'total_taxa', 'count': 3000}, ignore_index = True)
+
+
+    #### Make figure --> add bubbles to chorddiagram manually generated in R
     fig = px.scatter(count_df_narrow, x="taxa_cat", y="seqtype", size="count", text="count")
     fig.update_traces(textposition='top center')
     fig.show()
